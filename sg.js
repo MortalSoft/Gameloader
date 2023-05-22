b8e32e9b19179c9341867530f569255a31d275a2
RootSec <19959840+R00tS3c@users.noreply.github.com>
2023-05-22 05:55:30 +0200

    Update

--- sg.js
+++ sg.js
@@ -1,8 +1,9 @@
 "use strict";
 var Sg;
-(function (Sg) {
+(function(Sg) {
     var win = window;
     var netentScriptPath = "/gameinclusion/library/gameinclusion.js";
+
     function flashPlayerInstalled() {
         var UNDEF = "undefined";
         var OBJECT = "object";
@@ -12,11 +13,10 @@ var Sg;
         var nav = navigator;
         if (typeof nav.plugins !== UNDEF && typeof nav.plugins[SHOCKWAVE_FLASH] === OBJECT) {
             var d = nav.plugins[SHOCKWAVE_FLASH].description;
-            return (d && (typeof nav.mimeTypes !== UNDEF
-                && nav.mimeTypes[FLASH_MIME_TYPE]
-                && nav.mimeTypes[FLASH_MIME_TYPE].enabledPlugin));
-        }
-        else if (typeof win.ActiveXObject !== UNDEF) {
+            return (d && (typeof nav.mimeTypes !== UNDEF &&
+                nav.mimeTypes[FLASH_MIME_TYPE] &&
+                nav.mimeTypes[FLASH_MIME_TYPE].enabledPlugin));
+        } else if (typeof win.ActiveXObject !== UNDEF) {
             try {
                 var a = new ActiveXObject(SHOCKWAVE_FLASH_AX);
                 if (a) {
@@ -25,19 +25,34 @@ var Sg;
                         return true;
                     }
                 }
-            }
-            catch (e) {
-            }
+            } catch (e) {}
         }
         return false;
     }
-    function createIframe(target, uri, callbacks) {
-        var iframe = document.createElement("iframe");
-        iframe.src = uri;
-        iframe.setAttribute("allowfullscreen", "true");
-        target.appendChild(iframe);
-        return (callbacks.success)();
+
+    function createIframe(target, uri, token, callbacks) {
+        var headers = new Headers();
+        headers.append("Authorization", token);
+
+        fetch(uri, {
+                headers: headers
+            })
+            .then(function(response) {
+                if (response.ok) {
+                    var iframe = document.createElement("iframe");
+                    iframe.src = uri;
+                    iframe.setAttribute("allowfullscreen", "true");
+                    target.appendChild(iframe);
+                    return callbacks.success();
+                } else {
+                    throw new Error("Failed to fetch iframe: " + response.status);
+                }
+            })
+            .catch(function(error) {
+                return callbacks.error(error);
+            });
     }
+
     function startNetent(target, lo, callbacks) {
         var script = document.createElement("script");
         script.type = "text/javascript";
@@ -46,7 +61,7 @@ var Sg;
         var netentTarget = document.createElement("div");
         netentTarget.id = "sg-support-target-element-".concat(+new Date());
         target.appendChild(netentTarget);
-        script.onload = function () {
+        script.onload = function() {
             var nlo = lo.configuration;
             if (nlo.setTargetElement) {
                 delete nlo.setTargetElement;
@@ -57,28 +72,38 @@ var Sg;
                 nlo.width = win.innerWidth;
                 nlo.height = win.innerHeight;
             }
-            var success = function (netent) {
+            var success = function(netent) {
                 netent.resize(target.clientWidth, target.clientHeight);
                 if (lo.show_clock) {
-                    var handler = function () { return netent.call("showSystemClock", [], (function () {
-                    }), function (e) { return console.error(e); }); };
-                    netent.addEventListener("gameReady", handler, function (e) { return console.error(e); });
+                    var handler = function() {
+                        return netent.call("showSystemClock", [], (function() {}), function(e) {
+                            return console.error(e);
+                        });
+                    };
+                    netent.addEventListener("gameReady", handler, function(e) {
+                        return console.error(e);
+                    });
                 }
                 if (netent.resize) {
-                    win.onresize = function () { return netent.resize(target.clientWidth, target.clientHeight); };
+                    win.onresize = function() {
+                        return netent.resize(target.clientWidth, target.clientHeight);
+                    };
                 }
                 return (callbacks.success)();
             };
             return win.netent.launch(nlo, success, callbacks.error);
         };
-        script.onerror = function () { return callbacks.error("Failed to load script from '".concat(scriptUrl, "'")); };
+        script.onerror = function() {
+            return callbacks.error("Failed to load script from '".concat(scriptUrl, "'"));
+        };
         var body = document.getElementsByTagName("body")[0];
         return body.appendChild(script);
     }
+
     function startEGT(target, lo, callbacks) {
         var client = isMobile(lo.client_type) ? "mobile" : "desktop";
         lo.game_url += "&client=".concat(client);
-        var handler = function (event) {
+        var handler = function(event) {
             if (event.data && (event.data.command === "com.egt-bg.exit")) {
                 return win.location = lo.return_url;
             }
@@ -86,8 +111,9 @@ var Sg;
         win.addEventListener("message", handler, false);
         return detectLaunchTypeDeprecated(target, lo, callbacks);
     }
+
     function startGaming1(target, lo, callbacks) {
-        var handler = function (event) {
+        var handler = function(event) {
             if (event.data && (event.data === "closeGame")) {
                 return win.location = lo.return_url;
             }
@@ -95,24 +121,26 @@ var Sg;
         win.addEventListener("message", handler, false);
         return detectLaunchTypeDeprecated(target, lo, callbacks);
     }
+
     function startELK(target, lo, callbacks) {
         var device = isMobile(lo.client_type) ? "mobile" : "desktop";
         lo.game_url += "&device=".concat(device);
         return detectLaunchTypeDeprecated(target, lo, callbacks);
     }
+
     function startPlayngo(target, lo, callbacks) {
         var GameCommunicator = {
             source: undefined,
             targetOrigin: undefined,
-            init: function (element) {
+            init: function(element) {
                 window.addEventListener("message", this.processGameMessage.bind(this));
                 this.source = element.contentWindow;
                 this.targetOrigin = lo.origin;
             },
-            postMessage: function (data) {
+            postMessage: function(data) {
                 this.source.postMessage(data, this.targetOrigin);
             },
-            processGameMessage: function (e) {
+            processGameMessage: function(e) {
                 console.log("GameCommunicator received: ", e.data);
                 switch (e.data.type) {
                     case "reloadGame":
@@ -132,53 +160,66 @@ var Sg;
         playngoTarget.setAttribute("allowfullscreen", "true");
         playngoTarget.setAttribute("scrolling", "no");
         playngoTarget.setAttribute("noresize", "noresize");
-        playngoTarget.onload = function () {
+        playngoTarget.onload = function() {
             GameCommunicator.init(playngoTarget);
-            GameCommunicator.postMessage({ messageType: "addEventListener", eventType: "reloadGame" });
-            GameCommunicator.postMessage({ messageType: "addEventListener", eventType: "backToLobby" });
+            GameCommunicator.postMessage({
+                messageType: "addEventListener",
+                eventType: "reloadGame"
+            });
+            GameCommunicator.postMessage({
+                messageType: "addEventListener",
+                eventType: "backToLobby"
+            });
         };
-        playngoTarget.onerror = function () { return callbacks.error("Failed to load game from '".concat(uri, "'")); };
+        playngoTarget.onerror = function() {
+            return callbacks.error("Failed to load game from '".concat(uri, "'"));
+        };
         target.appendChild(playngoTarget);
         return (callbacks.success)();
     }
-    var isMobileDeprecated = function () { return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent); };
+    var isMobileDeprecated = function() {
+        return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent);
+    };
+
     function isMobile(clientType) {
         if (clientType === undefined) {
             return isMobileDeprecated();
         }
         return clientType == "mobile";
     }
+
     function detectLaunchTypeDeprecated(target, lo, callbacks) {
         var uri = gameURLFromOptions(lo);
         if (isMobileDeprecated()) {
             return win.location = uri;
-        }
-        else {
+        } else {
             return createIframe(target, uri, callbacks);
         }
     }
+
     function detectDeviceDeprecated(target, lo, callbacks) {
         if (isMobileDeprecated()) {
             return win.location = lo.mobile_url;
-        }
-        else {
+        } else {
             return createIframe(target, lo.desktop_url, callbacks);
         }
     }
+
     function gameURLFromOptions(lo) {
         if (lo.game_url) {
             return lo.game_url;
         }
         return flashPlayerInstalled() ? lo.flash_url : lo.html_url;
     }
+
     function startGame(options, sc, ec) {
         var callbacks = {
-            success: function () {
+            success: function() {
                 if (sc) {
                     return sc();
                 }
             },
-            error: function (e) {
+            error: function(e) {
                 if (ec) {
                     return ec(e);
                 }
@@ -194,7 +235,7 @@ var Sg;
             case "redirect":
                 return win.location = lo.game_url;
             case "iframe":
-                return createIframe(target, gameURLFromOptions(lo), callbacks);
+                return createIframe(target, gameURLFromOptions(lo), lo.token, callbacks);
             case "detect":
                 return detectLaunchTypeDeprecated(target, lo, callbacks);
             case "detect_device":
@@ -213,5 +254,7 @@ var Sg;
                 return Error("Unexpected launch strategy");
         }
     }
-    win.sg = { launch: startGame };
+    win.sg = {
+        launch: startGame
+    };
 })(Sg || (Sg = {}));
