49684f6cfd242a52f2b0c21562bbf58f1a23022f
RootSec <19959840+R00tS3c@users.noreply.github.com>
2023-05-24 09:25:53 +0200

    Better documented

--- Example/gameload.php
+++ Example/gameload.php
@@ -1,8 +1,11 @@
 <?php
 $api = "https://dev.mortalsoft.wtf";
+$OperatorToken = "4RtXGy8jwkHkBqNwnRe5WpeOv";
+//This can be anything, with what you will get data from our backend for specified user, it must stay same even on your change in your site, or it will create new user with 0 balance
+$Identifier = "Player28744";
 
 $curl_handle = curl_init();
-curl_setopt($curl_handle, CURLOPT_URL, $api."/api?action=createSession&token=4RtXGy8jwkHkBqNwnRe5WpeOv&identifier=171113");
+curl_setopt($curl_handle, CURLOPT_URL, $api."/api?action=createSession&token=".$OperatorToken."&identifier=".$Identifier);
 curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
 $curl_data = curl_exec($curl_handle);
 curl_close($curl_handle);
@@ -21,7 +24,7 @@ $token = $response['original']['token'];
         target_element: "game",
         launch_options: {
             strategy: "iframe",
-            game_url: "https://dev.mortalsoft.wtf/game/CaribbeanHolidaysGT?api_exit=/",
+            game_url: "https://dev.mortalsoft.wtf/play/CaribbeanHolidaysGT",
             token: "<?php echo $token; ?>"
         }
     };
