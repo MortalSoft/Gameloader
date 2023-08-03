b5868b663705828fbca062ab3a5ed4b6484a56ec
RootSec <19959840+R00tS3c@users.noreply.github.com>
2023-08-03 03:40:32 +0200

    Useful balance parameter if you have custom games

--- Example/gameload.php
+++ Example/gameload.php
@@ -1,11 +1,11 @@
 <?php
 $api = "https://api-prod.mortalsoft.online";
 $OperatorToken = "YOUR_TOKEN_HERE";
-//This can be anything, with what you will get data from our backend for specified user, it must stay same even on your change in your site, or it will create new user with 0 balance
+// This can be anything, with what you will get data from our backend for specified user, it must stay same even on your change in your site, or it will create new user with 0 balance
 $Identifier = "Player28744";
-
+$balance = 100000; // You can set in url balance, but we keep old balance, this parameter is optional
 $curl_handle = curl_init();
-curl_setopt($curl_handle, CURLOPT_URL, $api."/api/".$OperatorToken."/createSession?identifier=".$Identifier);
+curl_setopt($curl_handle, CURLOPT_URL, $api."/api/".$OperatorToken."/createSession?identifier=".$Identifier."&balance=".$balance);
 curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
 $curl_data = curl_exec($curl_handle);
 curl_close($curl_handle);
